d22f11a25532ca28b0cae37e9ed925ae25bf3a1a
XDPXIgamer <novatechnolagy.noreply@gmail.com>
2023-04-19 13:28:48 +1000

    1.0.0a3

--- e25.py
+++ e25.py
@@ -13,10 +13,10 @@ from tqdm import tqdm_gui
 import requests
 import zipfile
 
-ztrolibins = 'https://github.com/Ztrolix/E25/raw/main/bazt/scripts/ZtrolixLib.zip'
+ztrolibins = 'https://github.com/Ztrolix/E25/raw/main/bazt/scripts/temp001.zip'
 ztrolibinsr = requests.get(ztrolibins, allow_redirects=True)
 
-
+testf = open("bazt\\scripts\\test\\test.e25", "r")
 
 print(r"""  ______   ___    _____ 
  |  ____| |__ \  | ____|
@@ -26,14 +26,14 @@ print(r"""  ______   ___    _____
  |______| |____| |____/ 
                         
                         """)
-print("E25 [ Version 1.0.0a1 ]")
+print("E25 [ Version 1.0.0a3 ]")
 print("(c) E25. All rights reserved.")
 print()
 
 while True :
     a = str(input(": "))
     if (a == "bazt") :
-      print("bazt [ Version 1.0.0 ]")
+      print("bazt [ Version 1.1.0 ]")
     else :
       if "bazt install " in a :
         if (a == "bazt install ztrolib") :
@@ -48,10 +48,40 @@ while True :
           for i in tqdm(range(random.randint(10, 100))):
            ...
            sleep(random.randint(0, 2))
-          print("Failed Installment!")
+          print("Successfully Installed!")
         else :
           print("ERROR: Could not find a version that satisfies the requirement '" + str(a) + "' (from versions: none)")
       else:
-        print(r"""Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-NameError: name '""" + str(a) + "' is not defined")
+        if (a == "test.e25") :
+          print(testf)
+          try :
+            for x in testf:
+              print(x)
+          except :
+            print("Error: Read file 'test.e25'.")
+        else :
+          if (a == "run") :
+            print("Please input a '.e25' file.")
+            try :
+              filerun = str(input("> "))
+              runf = open(filerun, "r")
+            except :
+              print("Error: File '" + str(filerun) + "' has given a error.")
+            if ".e25" in filerun :
+              for y in runf:
+                  print(y)
+              while True :
+                runask = input("Do you want to run this file? [ Y or N ] ")
+                if (runask == "y") :
+                  print("Error: File '" + str(filerun) + "' has errors in its code.")
+                  break
+                else:
+                  if (runask == "n") :
+                    break
+                  else: 
+                    print("Error: '" + str(runask) + "' is not a valid input.")
+            else :
+              print("Error: File '" + str(filerun) + "' is not a '.e25' file.")
+          else  :
+            # new script here
+            print("Error: Name '" + str(a) + "' is not defined")
